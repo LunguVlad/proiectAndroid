b1690b52ed4cd6befaeae51dd1e6ffff8d2b4b44
Vlad <vladlungu2121@gmail.com>
2021-01-12 17:47:26 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/proiectAndroid/IntroActivity.java
@@ -0,0 +1,20 @@
+package com.example.proiectAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    //Atractii:
+    //1. Turnul Chindiei
+    //2. Manastirea Dealu
+    //3. Muzeul de istorie
+    //4. Casa Memoriala Grigore Alecsandrescu
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
---
+++ app/src/main/java/com/example/proiectAndroid/MapActivity.java
@@ -0,0 +1,15 @@
+package com.example.proiectAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    //poate
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
